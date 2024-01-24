0f458123db6269bd6ba18abb2d02da48ea858587
Anusha <154491643+Anushaa125@users.noreply.github.com>
2024-01-24 11:42:24 +0530

    day18.js

---
+++ Day18/day18.js
@@ -0,0 +1,41 @@
+let a = 5;
+       let b = 10;
+
+       console.log(a + b);       
+       console.log(a - b);
+       console.log(a * b);
+       console.log(a / b);
+       console.log(a % b);
+
+       a = 'Welcome';
+       b = 'Home';
+       console.log(a == b);
+
+       b = 'Welcome';
+       console.log(a == b)
+
+       a = 67;
+       b = '67';
+       console.log(a == b);
+       console.log(a === b);
+       console.log(a !== b);
+
+       b = 67;
+       console.log(a === b);
+       console.log(a < b);
+       console.log(a > b);
+       console.log(a <= b);
+       console.log(a >= b);
+       console.log(a != b);
+
+       a = 123;
+       b = 456;
+       console.log(a, b);
+
+       a += 5;
+       console.log(a);
+       console.log(a && b);
+       console.log(a || y);
+       console.log(a === b ? 'Hello' : 'World');
+       //here a=an integer number and b=an integer number since the condition is false it printed as world.
+       //if the statement is true then it would print as Hello
